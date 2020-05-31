71f7a24d34a83d82df83eab9f205529ce1bbe31e
koashima <48111257+koashima@users.noreply.github.com>
2020-05-30 18:23:47 -0700

    Gets buttons and sets up dummy functions

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+let counter = document.querySelector('.counter');
+
+let plus = document.querySelector('.plus');
+let minus = document.querySelector('.minus');
+
+
+plus.addEventListener('click', plusCounter);
+minus.addEventListener('click', minusCounter);
+
+
+function plusCounter() {
+
+}
+function minusCounter() {
+  console.log('hello')
+}
