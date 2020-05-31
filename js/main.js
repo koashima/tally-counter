8d7c9d27b164ac6fb0e0dff98eaea3781f5aa6fe
koashima <48111257+koashima@users.noreply.github.com>
2020-05-30 19:12:07 -0700

    finish mvp

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+let number = 0;  
 
 let counter = document.querySelector('.counter');
 
@@ -11,17 +12,24 @@ plus.addEventListener('click', plusCounter);
 minus.addEventListener('click', minusCounter);
 
 
-function plusCounter() {
-
-}
-function minusCounter() {
-  console.log('hello')
-}
 
 let init = () => {
-  input.value = 1
+  input.value = 1;
+  render()
 }
 
+let render = () => { 
+  counter.innerHTML = number
+}
 
+function plusCounter() {
+  number = number + parseInt(input.value);
+  render();
+}
+
+function minusCounter() {
+  number = number - parseInt(input.value);
+  render();
+}
 
 init()
